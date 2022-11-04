b732b685b986b059d3c9d5b67d1f6b3780bac42e
Anna <kowalewskajaanna@gmail.com>
2022-11-04 22:15:02 +0100

    refactor:task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -16,7 +16,7 @@ fs.readdir(path.join(__dirname, 'secret-folder'),
                     if (!stats.isDirectory()) {
                         let fileExtension = path.extname(file).slice(1, file.length);
                         let fileName = file.substring(0, file.indexOf(fileExtension) - 1);
-                        console.log(`${fileName} - ${fileExtension} - ${stats["size"]}`);
+                        console.log(`${fileName} - ${fileExtension} - ${(stats["size"]/1024).toFixed(0)}kb`);
                     }
                 });
             });
