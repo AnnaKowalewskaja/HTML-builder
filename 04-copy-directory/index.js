d026b5b4023b7e1d32b591a26c8f23219510817c
Anna <kowalewskajaanna@gmail.com>
2022-11-04 23:35:09 +0100

    fix:task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,57 @@
 const fs = require('fs');
 const path = require('node:path');
+const copyFile = require('node:fs/promises');
 
-fs.copyFile(path.join(__dirname, 'files'), path.join(__dirname,'files-copy'), err => {
-  
-   console.log('Файл успешно скопирован');
+let startFolder = path.join(__dirname, 'files');
+let finishFolder = path.join(__dirname, 'files-copy');
+
+fs.mkdir(finishFolder, {
+    recursive: true
+}, (err) => {
+    if (err) {
+        return console.error(err);
+    }
 });
+
+// fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+//     files.forEach(file => {
+//         copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file),constants.COPYFILE_EXCL);
+//         console.log('Complete!');
+
+//     });
+// });
+
+fs.readdir(startFolder,
+    (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            files.forEach(file => {
+                let fileName = file;
+                fs.copyFile(path.join(startFolder, file), path.join(finishFolder, fileName), (err) => {
+                    if (err) {
+                        console.log("Error Found:", err);
+                    }
+                });
+            });
+        }
+    });
+
+
+// fs.readdir(startFolder,
+//     (err, files) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             files.forEach(file => {
+//                 fs.stat(startFolder, function (err, stats) {
+//                     let fileName = file;
+//                     fs.copyFile(path.join(startFolder, file), path.join(finishFolder, fileName), (err) => {
+//                         if (err) {
+//                             console.log("Error Found:", err);
+//                         }
+//                     });
+//                 });
+//             });
+//         }
+//     });
